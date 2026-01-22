acfabd99e9b833cfabac5ef9abe36ca23647b1fa
Istiak Alam <168295675+Istiaq-Alam@users.noreply.github.com>
2026-01-22 14:58:00 +0600

    lab-02

--- Lab Task-02/main.cpp
+++ Lab Task-02/main.cpp
@@ -35,7 +35,7 @@ void DrawCircle(float cx, float cy, float rx,float ry, int num_segments)
 	glBegin(GL_TRIANGLE_FAN);
 	for(int ii = 0; ii < num_segments; ii++)
 	{
-		float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
+		float theta = (3.1415926f/2.0f) - 3.1415926f * float(ii) / float(num_segments);//get the current angle
 
 		float x = rx * cosf(theta);         //calculate the x component
 		float y = ry * sinf(theta);         //calculate the y component
@@ -47,8 +47,8 @@ void DrawCircle(float cx, float cy, float rx,float ry, int num_segments)
 }
 void init(void)
 {
-	glClearColor (0.0, 0.0, 0.0, 0.0);
-	 glOrtho(-500.0, 500.0, -300.0, 300.0, -1.0, 1.0);
+	glClearColor (0.0, 0.6, 0.2, 0.0);      // green color
+	 glOrtho(-500.0, 500.0, -500.0, 500.0, -1.0, 1.0);
 }
 
 void display(void)
@@ -59,8 +59,8 @@ void display(void)
 
 	glColor3f(0.7, 0.2, 0.3);
 
-DrawCircle(0,0,200,300,100);
- glPopMatrix();
+    DrawCircle(0,0,200,200,100);
+    glPopMatrix();
 
 	glFlush();
 }
@@ -69,8 +69,8 @@ int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
 	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize (1100, 600);
-	glutInitWindowPosition (500, 500);
+	glutInitWindowSize (800, 800);
+	glutInitWindowPosition (500, 100);
 	glutCreateWindow ("Circle");
 	init();
     glutDisplayFunc(display);
@@ -79,6 +79,8 @@ int main(int argc, char** argv)
 }
 
 
-
+// THETA = Q_start + (Q_end - Q_start) * (i/N)
+//       = 0 + (2pie - 0) * (i/N)
+//       = 2pie * (i/N)
 
 
